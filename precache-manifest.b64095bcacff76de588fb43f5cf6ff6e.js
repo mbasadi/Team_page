50559ede76cc8cb54b7aceb6df195f096bef00da
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-09 23:56:07 +0000

    Deploy mbasadi/Team_page to github.com/mbasadi/Team_page.git:gh-pages

---
+++ precache-manifest.b64095bcacff76de588fb43f5cf6ff6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3cea1dc3dfd5fb859b0b92a2a9fd6ba",
+    "url": "/Team_page/index.html"
+  },
+  {
+    "revision": "7c1eef8f7ebbd548288e",
+    "url": "/Team_page/static/css/main.f7432c39.chunk.css"
+  },
+  {
+    "revision": "681ad6dbd72fc0531897",
+    "url": "/Team_page/static/js/2.8a49dffc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Team_page/static/js/2.8a49dffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1eef8f7ebbd548288e",
+    "url": "/Team_page/static/js/main.a7eb7437.chunk.js"
+  },
+  {
+    "revision": "f0fcb6a22ff14341f516",
+    "url": "/Team_page/static/js/runtime-main.4544accc.js"
+  }
+]);
